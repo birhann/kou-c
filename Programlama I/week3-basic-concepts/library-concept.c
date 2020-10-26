b997c210530e74222c1a22cb6abe02cb9de722fc
birhann <birhankarahasan@outlook.com>
2020-10-26 09:47:09 +0300

    some library usage

---
+++ Programlama I/week3-basic-concepts/library-concept.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    int int_sayi;
+    float float_sayi;
+    char string;
+
+    // printf("Hello World!");
+    // scanf("%d", &int_sayi);
+
+    // time_t Time;
+    // srand((unsigned) time(&Time));
+    // printf("%d\n", rand() & 50);
+
+    // int_sayi = clock();
+    // printf("%d", int_sayi);
+
+    // time_t seconds; 
+    //  // Stores time seconds 
+    // time(&seconds); 
+    // printf("Seconds since January 1, 1970 = %ld\n", seconds); 
+
+    // double base, power, result;
+    // printf("Enter the base number: ");
+    // scanf("%lf", &base);
+    // printf("Enter the power raised: ");
+    // scanf("%lf",&power);
+    // result = pow(base,power);
+    // printf("%.1lf^%.1lf = %.2lf", base, power, result);
+
+    // char cdizi1[] = "";
+    // const char cdizi2[] = "Birhan K.";
+    // strcpy(cdizi1, cdizi2);
+    // printf("cdizi1 bellek icerigi: %s\n", cdizi1);
+
+    // printf("%d",isalpha('s'));
+
+
+    // printf("%d", int_sayi);
+    // printf("%f", float_sayi);
+    // printf("%d", string);
+
+    return 0;
+
+}
