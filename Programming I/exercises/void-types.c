e2ea0a75137db3b8edd41e2e627be649d98b6d35
birhann <birhankarahasan@outlook.com>
2020-11-28 00:11:05 +0300

    void types

---
+++ Programming I/exercises/void-types.c
@@ -0,0 +1,13 @@
+int main()
+{
+    // Function returns as void. A function with no return value has the return type as void.
+    void exit(int status);
+
+    // Function arguments as void A function with no parameter can accept a void.
+    int rand(void);
+
+    // Pointers to void. A pointer of type void * represents the address of an object, but not its type.
+    void *malloc(size_t size);
+
+    return 0;
+}
