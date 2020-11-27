c28f6628331a2e6e97bb1c1c8f6438cb07be0a84
birhann <birhankarahasan@outlook.com>
2020-11-28 01:14:25 +0300

    function call by reference

---
+++ Programming I/exercises/functions-call-by-reference.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// function declaration
+void swap(int *x, int *y);
+
+int main()
+{
+    int a = 100;
+    int b = 200;
+
+    printf("Before swap, value of a : %d\n", a);
+    printf("Before swap, value of b : %d\n", b);
+
+    swap(&a, &b);
+
+    printf("After swap, value of a : %d\n", a);
+    printf("After swap, value of b : %d\n", b);
+
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
+    return;
+}
