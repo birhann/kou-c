bb75978777d0953e92e34e011b6a08a79dfd8106
birhann <birhankarahasan@outlook.com>
2020-11-30 23:25:26 +0300

    structure usage

--- Programming I/exercises/defining-structure.c
+++ Programming I/exercises/defining-structure.c
@@ -11,6 +11,18 @@ struct Books
 
 int main()
 {
+    struct Books Book1;
+    struct Books Book2;
+
+    strcpy(Book1.title, "C Programming");
+    strcpy(Book1.author, "Birhan Karahasan");
+    strcpy(Book1.subject, "Practise");
+    Book1.book_id = 12345678;
+
+    printf("Book 1 title : %s\n", Book1.title);
+    printf("Book 1 author : %s\n", Book1.author);
+    printf("Book 1 subject : %s\n", Book1.subject);
+    printf("Book 1 book_id : %d\n", Book1.book_id);
 
     return 0;
 }
